1b8df4f63bf7af48db4f2fc1557e6a39d30c8149
Shunsuke Saito <shunsuke.saito16@gmail.com>
2016-07-14 16:27:47 -0700

    update script

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopenblas-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install libopenblas-dev git unzip
+
+wget https://raw.githubusercontent.com/jayrambhia/Install-OpenCV/master/Ubuntu/2.4/opencv2_4_9.sh
+chmod +x opencv2_4_9.sh 
+./opencv2_4_9.sh 
+
+wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
+bash Anaconda-2.1.0-Linux-x86.sh
+
+sudo apt-get install libboost-all-dev
+
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+
+pip install protobuf
+
+make all
