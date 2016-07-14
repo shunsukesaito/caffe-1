672113e1abc005b9ddb23d89e004d470fe810ae0
Shunsuke Saito <shunsuke.saito16@gmail.com>
2016-07-14 16:14:48 -0700

    add script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopenblas-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
