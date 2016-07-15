7f902875f9e10798c39ca1b3d0af79e1fbe7ff01
Shunsuke Saito <shunsuke.saito16@gmail.com>
2016-07-14 19:58:59 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get install --no-install-recommends libboost-all-dev
 
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
-sudo apt-get install libatlas-base-dev
+sudo apt-get install libatlas-base-dev libopenblas-dev
 
-make all
+cd build
+cmake -D CPU_ONLY=1 ../
+make -j 5
