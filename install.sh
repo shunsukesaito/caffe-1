1a0c0420c6e7bd9ff42067558db9a8fca0b22722
Shunsuke Saito <shunsuke.saito16@gmail.com>
2016-07-14 17:12:43 -0700

    split script

--- install.sh
+++ install.sh
@@ -1,39 +1,7 @@
-sudo apt-get install libopenblas-dev git unzip
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
 
-cd ~
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-git checkout 3.1.0
-
-cd ~
-git clone https://github.com/Itseez/opencv_contrib.git
-cd opencv_contrib
-git checkout 3.1.0
-
-cd ~/opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D CMAKE_INSTALL_PREFIX=/usr/local \
--D INSTALL_C_EXAMPLES=OFF \
--D INSTALL_PYTHON_EXAMPLES=ON \
--D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
--D BUILD_EXAMPLES=OFF \
--D BUILD_TEST=OFF ..
-
-make -j4
-
-sudo make install
-sudo ldconfig
-
-wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
-bash Anaconda-2.1.0-Linux-x86.sh
-
-sudo apt-get install libboost-all-dev
-
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
-
-pip install protobuf
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install libatlas-base-dev
 
 make all
