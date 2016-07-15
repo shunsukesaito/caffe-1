55b964194aa8cd5c5f8367afec208a410dd5ad2c
Shunsuke Saito <shunsuke.saito16@gmail.com>
2016-07-14 17:05:53 -0700

    update install

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 sudo apt-get install libopenblas-dev git unzip
 
-wget https://raw.githubusercontent.com/jayrambhia/Install-OpenCV/master/Ubuntu/2.4/opencv2_4_9.sh
-chmod +x opencv2_4_9.sh 
-./opencv2_4_9.sh 
+cd ~
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+git checkout 3.1.0
+
+cd ~
+git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.1.0
+
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D INSTALL_C_EXAMPLES=OFF \
+-D INSTALL_PYTHON_EXAMPLES=ON \
+-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+-D BUILD_EXAMPLES=OFF \
+-D BUILD_TEST=OFF ..
+
+make -j4
+
+sudo make install
+sudo ldconfig
 
 wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
 bash Anaconda-2.1.0-Linux-x86.sh
